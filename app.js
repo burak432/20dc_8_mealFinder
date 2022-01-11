fedb70e025ad57b21a25d3e4cd3327fb01f6e9ed
Burak Atilla <burak.atilla@gmail.com>
2022-01-11 20:27:29 +0300

    2 | search function works. it display meals into dom. but click to display single meal needs to be done

--- app.js
+++ app.js
@@ -19,22 +19,25 @@ function searchMeal(e) {
   //check empty seach
   if (term.trim()) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
         resultsHeading.innerHTML = `<h4>Search results for : ${term}</h4>`;
 
         if (data.meals === null) {
           resultsHeading.innerHTML = `<h4>Sorry. No results for : ${term}</h4>`;
         } else {
-          mealsEl.innerHTML = data.meals.map(item => `
+          mealsEl.innerHTML = data.meals
+            .map(
+              (item) => `
           <div class="meal">
           <img src="${item.strMealThumb}" alt="${item.strMeal}" />
           <div class="meal-info" data-mealID="${item.idMeal}">
             <h3>${item.strMeal}</h3>
           </div>
         </div>`
-          ).join("");
+            )
+            .join("");
         }
       });
 
@@ -45,5 +48,31 @@ function searchMeal(e) {
   }
 }
 
+//function for fetch meal by id num
+function getMealByID(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      addMealtoDom(meal);
+    });
+}
+
+function addMealtoDom(meal) {}
+
 //event listeners
 submit.addEventListener("submit", searchMeal);
+mealsEl.addEventListener("click", (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      //event clicks returns whatever has the classname of "meal-info"
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute("data-mealID");
+    getMealByID(mealID);
+  }
+});
