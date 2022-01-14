bbdc28e67b3c4631c0cc785ce9faa3311f7e07ce
Burak Atilla <burak.atilla@gmail.com>
2022-01-14 22:24:47 +0300

    5 | fixed errors, added display meal recipie when clicked on a single meal, but needs to rework

--- app.js
+++ app.js
@@ -54,11 +54,42 @@ function getMealByID(mealID) {
     .then((res) => res.json())
     .then((data) => {
       const meal = data.meals[0];
-      addMealtoDom(meal);
+      addMealToDOM(meal);
     });
 }
 
-function addMealtoDom(meal) {}
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  singleMeal.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
+  `;
+}
+
 
 //event listeners
 submit.addEventListener("submit", searchMeal);
